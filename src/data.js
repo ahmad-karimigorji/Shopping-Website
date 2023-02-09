e6339448d2b1a0335374acae29d201c8b7b78c06
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-02-09 10:58:34 +0330

    initial commit

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const productsData = [
+  {
+    id: 1,
+    name: "",
+    price: 23,
+    imageURL: "https://s2.uupload.ir/files/shop_01_14md.jpg",
+  },
+  {
+    id: 2,
+    name: "",
+    price: 95,
+    imageURL: "https://s2.uupload.ir/files/shop_02_m1h5.jpg",
+  },
+  {
+    id: 3,
+    name: "",
+    price: 77,
+    imageURL: "https://s2.uupload.ir/files/shop_03_a1nk.jpg",
+  },
+  {
+    id: 4,
+    name: "",
+    price: 130,
+    imageURL: "https://s2.uupload.ir/files/shop_04_pym1.jpg",
+  },
+  {
+    id: 5,
+    name: "",
+    price: 250,
+    imageURL: "https://s2.uupload.ir/files/shop_05_ws70.jpg",
+  },
+  {
+    id: 6,
+    name: "",
+    price: 287,
+    imageURL: "https://s2.uupload.ir/files/shop_06_qe.jpg",
+  },
+  {
+    id: 7,
+    name: "",
+    price: 23,
+    imageURL: "https://s2.uupload.ir/files/shop_07_m7gy.jpg",
+  },
+  {
+    id: 8,
+    name: "",
+    price: 80,
+    imageURL: "https://s2.uupload.ir/files/shop_08_6ufb.jpg",
+  },
+  {
+    id: 9,
+    name: "",
+    price: 54,
+    imageURL: "https://s2.uupload.ir/files/shop_09_e6yh.jpg",
+  },
+];
