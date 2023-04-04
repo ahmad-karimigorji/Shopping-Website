8f5fb43b78b45ba7f8090c3a770baccc97ecf278
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-04-04 17:07:46 +0330

    fetch data

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,14 +2,34 @@ import {
   useCart,
   useCartDispatcher,
 } from "../context/CartProvider/CartProvider";
-import { productsData } from "../data";
 import { addToCart, removeFromCart } from "../context/CartProvider/cartActions";
 import { toast } from "react-toastify";
+import { useState, useEffect } from "react";
+import { getProducts } from "../services/productsService";
+import LoadingPage from "../component/loading/LoadingPage";
 
 const Shop = () => {
+  const [loading, setLoading] = useState(false);
   const dispatch = useCartDispatcher();
   const { cart } = useCart();
-  console.log(cart);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await getProducts();
+      setProducts(data);
+      setLoading(false);
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
   const addToCartHandler = (product) => {
     dispatch(addToCart(product));
@@ -19,28 +39,36 @@ const Shop = () => {
     dispatch(removeFromCart(product));
   };
 
-  const products = productsData;
+  if (loading) return <LoadingPage />;
+
   return (
-    <section className="">
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,290px))] justify-center gap-y-10 gap-x-6">
-        {products.map((product) => (
-          <div className="border border-gray-200" key={product.id}>
-            <img src={product.imageURL} alt={product.name} />
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,290px))] justify-center gap-y-10 gap-x-6">
+      {products.length > 0 &&
+        products.map((product) => (
+          <div
+            className="border border-gray-200 rounded overflow-hidden"
+            key={product._id}
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-[270px] object-cover"
+            />
             <div className="flex justify-between items-center px-2 h-20">
               <div className="font-semibold	">
                 <p>{product.name}</p>
                 <p className="text-indigo-600">$ {product.price}</p>
               </div>
-              {cart.some((item) => item.id === product.id) ? (
+              {cart.some((item) => item._id === product._id) ? (
                 <button
-                  className="border border-indigo-600 rounded-md text-indigo-600 px-2 py-1"
+                  className="border border-indigo-600 rounded text-indigo-600 px-2 py-1"
                   onClick={() => removeFromCartHandler(product)}
                 >
                   remove
                 </button>
               ) : (
                 <button
-                  className="bg-indigo-600 rounded-md text-white px-2 py-1.5"
+                  className="bg-indigo-600 rounded text-white px-2 py-1.5"
                   onClick={() => addToCartHandler(product)}
                 >
                   Add To Cart
@@ -49,8 +77,7 @@ const Shop = () => {
             </div>
           </div>
         ))}
-      </div>
-    </section>
+    </div>
   );
 };
 
